a676bc602fc6227de8b74d8a5d73e515e5140dd0
flydog98 <guinnessoverflow@gmail.com>
2025-03-25 20:34:10 +0900

    feat: 테스트 위해 다시 상향

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -102,8 +102,8 @@ module "mig_ai" {
   source                  = "../../modules/mig"
   name                    = "babpat-ai"
   instance_template       = "babpat-ai-template"
-  min_replicas            = 1
-  max_replicas            = 2
+  min_replicas            = 2
+  max_replicas            = 5
   target_cpu_utilization  = 0.6
   region                  = var.region
   ports                   = ["8000"]
