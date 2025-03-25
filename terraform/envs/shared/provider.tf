8b10f07edbe0a1f3f6a7f60ffba69c6bfec4c685
flydog98 <guinnessoverflow@gmail.com>
2025-03-25 20:07:56 +0900

    CI/CD: provider 재수정

--- terraform/envs/shared/provider.tf
+++ terraform/envs/shared/provider.tf
@@ -1,1 +1,17 @@
+provider "google" {
+  project = var.project
+  region  = var.region
 
+  # CI 환경에서는 환경변수 GOOGLE_APPLICATION_CREDENTIALS 사용
+  credentials = can(var.credential_file_path) && length(var.credential_file_path) > 0 ? file(var.credential_file_path) : null
+}
+
+terraform {
+  required_version = ">= 1.0"
+
+  # 원하는 경우, 백엔드(예: remote backend, GCS 등) 설정도 여기서 가능
+  # backend "gcs" {
+  #   bucket = "my-terraform-state-bucket"
+  #   prefix = "some-prefix"
+  # }
+}
